9ef45a3ce174b41cd5b37b3cc96eb5c004ebc792
rhnyewale <46860673+rhnyewale@users.noreply.github.com>
2020-07-31 20:50:34 -0400

    Update

---
+++ Day 7 - Python Packages and Libraries/Lec.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 01:12:16 2020
+
+@author: rhnye
+"""
+str1 = 'Welcome'
+print(str1)
+import numpy as np
+
+
+data1 = [[1,2,3,4],[5,6,7,8]]
+
+arr1 = np.array(data1)
+
+arr1.ndim
